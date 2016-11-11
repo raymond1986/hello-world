848e13503a20275505d45205eb2432d2204cb479
raymond <raymond.ma@goertek.com>
2016-11-11 10:39:13 +0800

    Add PrintHello() for hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+void PrintHello();
+
 /* add comments. */
 int main()
 {
 
     std::cout << "Hello world." << std::endl;
     return 1;
+}
+
+void PrintHello()
+{
+    std::cout << "Hello." << std::endl;
 }
